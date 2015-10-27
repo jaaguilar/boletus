63b7a8be61006bd723ba77b5a8ee25889b940d35
jaaguilar <juanantonioaguilar@gmail.com>
2015-10-27 10:31:30 -0700

    config data transfer limit

--- middleware/index.js
+++ middleware/index.js
@@ -3,14 +3,16 @@ var logger = require('express-logger');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
+var config = require('../config');
 
 module.exports = function (app) {
 	app.use(logger({path: "./logfile.txt"}));
 
 	app.use(cookieParser());
 	app.use(session({secret: 'boletus con bogavante', resave: true, saveUninitialized: true}));
-	app.use(bodyParser.urlencoded({ extended: true }));
-	app.use(bodyParser.json());
+	//increase limit to avoid "413 Request Entity Too Large" error
+	app.use(bodyParser.urlencoded({ extended: true, limit: config.limitDataTransferSize }));
+	app.use(bodyParser.json({limit: config.limitDataTransferSize }));
 	//expose session to views
 	app.use(function (req, res, next){
 		res.locals.session = req.session;
